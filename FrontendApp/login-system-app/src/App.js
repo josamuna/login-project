b295f0b3ec62e4f82e443c7eda3932aad076bb87
Josue Isamuna Nkembo <josamuna2009@gmail.com>
2023-08-23 16:33:00 +0200

    Formatting code properly with Prettier

--- FrontendApp/login-system-app/src/App.js
+++ FrontendApp/login-system-app/src/App.js
@@ -1,28 +1,31 @@
 import React, { useState, useEffect } from 'react';
-import  Axios from 'axios';
+import Axios from 'axios';
 import './App.css';
-import {Table} from 'reactstrap';
-import {NotificationContainer, NotificationManager} from 'react-notifications'
+import { Table } from 'reactstrap';
+import {
+  NotificationContainer,
+  NotificationManager,
+} from 'react-notifications';
 
 function App() {
   const [userData, setUserData] = useState([]);
-  const url_get = "http://127.0.0.1:8000/api/loginsystem/";
-  
-  // Fetch data from Django api using an expected url 
-  const fetchDataFromApi = async(url_get)=>{
-    try{
+  const url_get = 'http://127.0.0.1:8000/api/loginsystem/';
+
+  // Fetch data from Django api using an expected url
+  const fetchDataFromApi = async (url_get) => {
+    try {
       const response = await Axios.get(url_get);
       console.log('Response is :', response.data);
       setUserData(response.data);
-    }catch(error){
-      console.log(`Something went wrong:${ error }`);
+    } catch (error) {
+      console.log(`Something went wrong:${error}`);
       NotificationManager.warning('Failed to load data!', 'Loading Data', 6000);
-    }    
-  }
+    }
+  };
 
-  useEffect(() =>{
-    fetchDataFromApi(url_get)
-  }, [])
+  useEffect(() => {
+    fetchDataFromApi(url_get);
+  }, []);
 
   return (
     <div className="App">
@@ -37,21 +40,21 @@ function App() {
                 <th>Password</th>
               </tr>
             </thead>
-            {userData.map((item)=>{
-              return(
+            {userData.map((item) => {
+              return (
                 <tbody key={item.id}>
-                    <tr key={item.id}>
-                      <td>{item.id}</td>
-                      <td>{item.fullname}</td>
-                      <td>{item.email}</td>
-                      <td>{item.password}</td>
-                    </tr>
+                  <tr key={item.id}>
+                    <td>{item.id}</td>
+                    <td>{item.fullname}</td>
+                    <td>{item.email}</td>
+                    <td>{item.password}</td>
+                  </tr>
                 </tbody>
-              )
+              );
             })}
           </Table>
-        </div> 
-        <NotificationContainer />       
+        </div>
+        <NotificationContainer />
       </header>
     </div>
   );
