0f566351b03f71d99d1aa20eb6730222d6fea7e2
Josue Isamuna Nkembo <josamuna2009@gmail.com>
2024-06-11 15:36:44 +0200

    Updating dependencies to solve security issues

--- FrontendApp/login-system-app/src/index.js
+++ FrontendApp/login-system-app/src/index.js
@@ -4,7 +4,6 @@ import "bootstrap/dist/css/bootstrap.css";
 import "react-notifications/lib/notifications.css";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -16,4 +15,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
